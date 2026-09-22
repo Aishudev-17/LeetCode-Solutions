2202eed7f0615f09e5a00cddcd22bcaecd6d9e3a
Aishudev-17 <sachuaishwarya17@gmail.com>
2026-09-22 14:47:55 +0530

    Add Medium Java Solution for 2299. Merge Nodes in Between Zeros from LeetCode

--- LeetCode/Patterns/Linked List/2299. Merge Nodes in Between Zeros/Solution.java
+++ LeetCode/Patterns/Linked List/2299. Merge Nodes in Between Zeros/Solution.java
@@ -10,6 +10,24 @@
  */
 class Solution {
     public ListNode mergeNodes(ListNode head) {
+        ListNode p1=head.next;
+        ListNode p2=head;
+        int sum=0;
+    
+        while(p1!=null){
+            if(p1.val!=0){
+                sum+=p1.val;
+            }
+        else{
+            p2=p2.next;
+            p2.val=sum;
+            sum=0;
+
+        }
+        p1=p1.next;
+        }
+        p2.next=null;
+        return head.next;
         
     }
 }
