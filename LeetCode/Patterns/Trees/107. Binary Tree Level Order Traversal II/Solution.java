6745e677f14636f3f3a36a751031954acaaccf49
Aishudev-17 <sachuaishwarya17@gmail.com>
2026-09-26 22:35:26 +0530

    Add Medium Java Solution for 107. Binary Tree Level Order Traversal II from LeetCode

---
+++ LeetCode/Patterns/Trees/107. Binary Tree Level Order Traversal II/Solution.java
@@ -0,0 +1,37 @@
+class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+
+        if(root == null){
+            return ans;
+        }
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+
+        while(!q.isEmpty()){
+            int n = q.size();
+            List<Integer> list = new ArrayList<>();
+
+            for(int i = 0; i < n; i++){
+                TreeNode node = q.poll();
+
+                list.add(node.val);
+
+                if(node.left != null){
+                    q.add(node.left);
+                }
+
+                if(node.right != null){
+                    q.add(node.right);
+                }
+            }
+
+            ans.add(list);
+        }
+
+        Collections.reverse(ans);
+
+        return ans;
+    }
+}
